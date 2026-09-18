9e09659639102fb671e418448fa706b4193b4ca5
Carlos <carlos>
2026-09-18 16:20:45 +0200

    add sqllite config

--- angular-nestjs-contract-first/applications/backend/api/src/app/app.module.ts
+++ angular-nestjs-contract-first/applications/backend/api/src/app/app.module.ts
@@ -1,9 +1,19 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqljs',
+      location: join(process.cwd(), 'data', 'db.sqlite'),
+      autoSave: true,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
